f7dc37e261930b54716170d7058cd2b9274ef81c
signal-chain25 <signalchain.tools@gmail.com>
2025-06-13 22:10:41 +0000

    Fix collection name from 'artist' to 'artists'

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const pb = new PocketBase("https://tapedrop.pockethost.io");
 const artistList = {
   artists: [],
   async init() {
-    const records = await pb.collection('artist').getFullList({
+    const records = await pb.collection('artists').getFullList({
       expand: 'label',
       sort: 'name',
     });
