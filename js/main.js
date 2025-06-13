80fe8a8ab06f925e1022888acb08323c8c95edb6
signal-chain25 <signalchain.tools@gmail.com>
2025-06-13 22:31:54 +0000

    Remove CommonJS module error and fix PocketBase init

--- js/main.js
+++ js/main.js
@@ -1,22 +1,12 @@
-// Load PocketBase directly via IIFE
-(async () => {
-  const script = document.createElement("script");
-  script.src = "https://unpkg.com/pocketbase@0.18.4/dist/pocketbase.umd.js";
-  script.onload = init;
-  document.head.appendChild(script);
+const pb = new PocketBase("https://tapedrop.pockethost.io");
 
-  function init() {
-    const pb = new PocketBase("https://tapedrop.pockethost.io");
-
-    window.artistList = {
-      artists: [],
-      async init() {
-        const records = await pb.collection('artists').getFullList({
-          expand: 'label',
-          sort: 'name',
-        });
-        this.artists = records;
-      }
-    };
+const artistList = {
+  artists: [],
+  async init() {
+    const records = await pb.collection('artist').getFullList({
+      expand: 'label',
+      sort: 'name',
+    });
+    this.artists = records;
   }
-})();
+};
