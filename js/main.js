0f3ee9f8742d27f7a1506198106b1a88c86940dc
signal-chain25 <signalchain.tools@gmail.com>
2025-06-13 22:20:42 +0000

    Inline PocketBase to fix load issue on Netlify

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-const pb = new PocketBase("https://tapedrop.pockethost.io");
+// Load PocketBase directly via IIFE
+(async () => {
+  const script = document.createElement("script");
+  script.src = "https://unpkg.com/pocketbase@0.18.4/dist/pocketbase.umd.js";
+  script.onload = init;
+  document.head.appendChild(script);
 
-const artistList = {
-  artists: [],
-  async init() {
-    const records = await pb.collection('artists').getFullList({
-      expand: 'label',
-      sort: 'name',
-    });
-    this.artists = records;
+  function init() {
+    const pb = new PocketBase("https://tapedrop.pockethost.io");
+
+    window.artistList = {
+      artists: [],
+      async init() {
+        const records = await pb.collection('artists').getFullList({
+          expand: 'label',
+          sort: 'name',
+        });
+        this.artists = records;
+      }
+    };
   }
-};
+})();
